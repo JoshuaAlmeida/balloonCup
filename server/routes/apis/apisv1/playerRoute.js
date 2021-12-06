54ee5eb812eaadba63b5baea8e8778f0a09aa08e
JoshuaAlmeida <joshua93x@gmail.com>
2021-12-06 12:27:49 +0000

    Create skeleton routes

---
+++ server/routes/apis/apisv1/playerRoute.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const router = require("express").Router()
+const Model = require("mongoose").model("Player")
+
+router.get('/', function(req, res, next){
+
+})
+
+router.post('/', function(req, res, next){
+
+})
+
+router.put('/', function(req, res, next){
+
+})
+
+router.delete('/', function(req, res, next){
+    
+})
